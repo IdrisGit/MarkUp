3116badfbfc60edcf99dfcc176a4eff1fb932c1d
idrisGit <idris.gadi01@gmail.com>
2023-05-27 16:40:01 +0530

    added basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import NotesList from './pages/NotesList';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <NotesList />
+    },
+    {
+      path: '/new',
+      element: <h1>New Note</h1>
+    },
+    {
+      path: '/:id',
+      element: <Outlet />,
+      children: [
+        {
+          index: true,
+          element: <h2>Note</h2>
+        },
+        {
+          path: 'edit',
+          element: <h2>Edit</h2>
+        }
+      ]
+    },
+    {
+      path: "*",
+      element: <Navigate to='/' replace />
+    }
+  ])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/NotesList.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+interface NotesListProp {
+
+}
+
+const NotesList: React.FC<NotesListProp> = (props) => {
+    return(
+        <div>
+            <h1>Notes List</h1>
+        </div>
+    )
+};
+
+export default NotesList;

faca9d75d07c3eb5366283b769a6a56aaab5d197
idrisGit <idris.gadi01@gmail.com>
2023-06-10 11:30:26 +0530

    base fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/React-NoteTaker",
+  base:"",
   plugins: [react()],
   server:{
     port: 8080

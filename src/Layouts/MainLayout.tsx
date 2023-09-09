b60ebc72b2370fa164c2087819a1ed9b10101e04
idrisGit <idris.gadi01@gmail.com>
2023-09-09 14:50:15 +0530

    added switch to the layout

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,7 +1,20 @@
 import { Outlet } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+import ThemeSwitch from '../components/shared/ThemeSwitch/ThemeSwitch';
 
 const MainLayout = () => {
-  return <Outlet />;
+  return (
+    <Container>
+      <Row>
+        <Col className='d-flex justify-content-end pb-md-3'>
+          <ThemeSwitch />
+        </Col>
+      </Row>
+      <Row>
+        <Outlet />
+      </Row>
+    </Container>
+  );
 };
 
 export default MainLayout;

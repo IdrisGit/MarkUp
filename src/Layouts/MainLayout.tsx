70582d5ba599c0a6e0ae64723921d14019073bfc
idrisGit <idris.gadi01@gmail.com>
2023-12-09 11:47:22 +0530

    update: updated app logo and added the icon in the navbar

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,7 +1,8 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTheme } from '../context/ThemeContext';
 import ThemeSwitch from '../components/shared/ThemeSwitch/ThemeSwitch';
+import Logo from '../../public/logo.svg?react';
 
 const MainLayout = () => {
   const { theme } = useTheme();
@@ -16,6 +17,11 @@ const MainLayout = () => {
       }}
     >
       <Row>
+        <Col className='d-flex justify-content-start pt-2 p-md-3'>
+          <Link to={'/'}>
+            <Logo />
+          </Link>
+        </Col>
         <Col className='d-flex justify-content-end pt-2 p-md-3'>
           <ThemeSwitch />
         </Col>

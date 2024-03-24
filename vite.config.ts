1592e9d622dd77e1555d8f2d9f7642cb7a224ea6
idrisGit <idris.gadi01@gmail.com>
2024-03-24 12:34:02 +0530

    setup: vitest react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
+import type { InlineConfig } from 'vitest';
+
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+type ViteConfig = UserConfig & { test: InlineConfig };
+const config: ViteConfig = {
   base: '/MarkUp',
   plugins: [react(), svgr()],
   server: {
     port: 8080,
   },
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.ts',
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(config);

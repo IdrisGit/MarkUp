af8a3a1fef979faf67395a34d15f585ef28595cf
idrisGit <idris.gadi01@gmail.com>
2024-03-24 09:34:21 +0530

    update: base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-NoteTaker',
+  base: '/MarkUp',
   plugins: [react(), svgr()],
   server: {
     port: 8080,

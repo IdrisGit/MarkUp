e77a1ac2908756e4cca5c862ee2ed66759cf4b18
idrisGit <idris.gadi01@gmail.com>
2024-06-18 23:06:44 +0530

    style: driverjs popover styling

--- src/features/Topbar/components/Topbar.tsx
+++ src/features/Topbar/components/Topbar.tsx
@@ -6,6 +6,7 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 import { FaGithub } from 'react-icons/fa';
 import { driver } from 'driver.js';
 import 'driver.js/dist/driver.css';
+import '@features/Topbar/components/driverjs.css';
 
 interface TopbarProps {
   setIsNavOpen: Dispatch<SetStateAction<boolean>>;
@@ -14,93 +15,103 @@ interface TopbarProps {
 export const Topbar: React.FC<TopbarProps> = ({ setIsNavOpen }) => {
   const { colorMode, toggleColorMode } = useColorMode();
 
-  const driverObj = driver({
-    showProgress: true,
-    stagePadding: 6,
-    stageRadius: 5,
-    steps: [
-      {
-        element: '#note-list-search-filter',
-        popover: {
-          title: 'Notes Search',
-          description: 'You can search and filter through your notes by title and/or tags.',
+  const handleStartTour = () => {
+    const driverObj = driver({
+      showProgress: true,
+      stagePadding: 6,
+      stageRadius: 5,
+      overlayColor: '#000000AA',
+      overlayOpacity: 0.9,
+      popoverClass: 'driverjs-theme',
+      steps: [
+        {
+          element: '#note-list-search-filter',
+          popover: {
+            title: 'Notes Search',
+            description: 'You can search and filter through your notes by title and/or tags.',
+          },
         },
-      },
-      {
-        element: '#add-edit-tags-button',
-        popover: { title: 'Edit Tags', description: 'You can add/edit your tags.' },
-      },
-      {
-        element: '#note-card',
-        popover: { title: 'Note Card', description: 'Notes details.' },
-      },
-      {
-        element: '#note-title',
-        popover: { title: 'Note Title', description: 'Title of your note.' },
-      },
-      {
-        element: '#note-tag-list',
-        popover: { title: 'Tags', description: 'All the tags currently attached to your note.' },
-      },
-      {
-        element: '#create-new-note-button',
-        popover: { title: 'Create New Note', description: 'Click here to open a new blank note.' },
-      },
-      {
-        element: '#note-form-title',
-        popover: { title: 'Note Title', description: 'You can enter title of your note here.' },
-      },
-      {
-        element: '#note-form-tag-select',
-        popover: {
-          title: 'Create New Note',
-          description:
-            'You can select all the tags you want to attach to your note. You can also create new tags.',
+        {
+          element: '#add-edit-tags-button',
+          popover: { title: 'Edit Tags', description: 'You can add/edit your tags.' },
         },
-      },
-      {
-        element: '#note-markdown',
-        popover: {
-          title: 'Note Body',
-          description: 'You can note down all that you desire here. (Markdown Supported)',
+        {
+          element: '#note-card',
+          popover: { title: 'Note Card', description: 'Notes details.' },
         },
-      },
-      {
-        element: '#note-form-save',
-        popover: {
-          title: 'Save Note',
+        {
+          element: '#note-title',
+          popover: { title: 'Note Title', description: 'Title of your note.' },
         },
-      },
-      {
-        element: '#note-form-cancel',
-        popover: {
-          title: 'Cancel',
-          description: 'If you want to cancel the note.',
+        {
+          element: '#note-tag-list',
+          popover: { title: 'Tags', description: 'All the tags currently attached to your note.' },
         },
-      },
-      {
-        element: '#theme-switch-button',
-        popover: {
-          title: 'Theme Switch',
-          description: 'You can switch between Light and Dark mode. (Default: System Preference)',
+        {
+          element: '#create-new-note-button',
+          popover: {
+            title: 'Create New Note',
+            description: 'Click here to open a new blank note.',
+          },
         },
-      },
-      {
-        element: '#github-link',
-        popover: {
-          title: 'GitHub Link',
-          description:
-            'You can checkout the GitHub repo by clicking here. If you like the project you can also star &#11088; it there.',
+        {
+          element: '#note-form-title',
+          popover: { title: 'Note Title', description: 'You can enter title of your note here.' },
         },
-      },
-      {
-        popover: {
-          title: 'MarkUp',
-          description: 'You can play around the app and happy note taking :) .',
+        {
+          element: '#note-form-tag-select',
+          popover: {
+            title: 'Create New Note',
+            description:
+              'You can select all the tags you want to attach to your note. You can also create new tags.',
+          },
         },
-      },
-    ],
-  });
+        {
+          element: '#note-markdown',
+          popover: {
+            title: 'Note Body',
+            description: 'You can note down all that you desire here. (Markdown Supported)',
+          },
+        },
+        {
+          element: '#note-form-save',
+          popover: {
+            title: 'Save Note',
+          },
+        },
+        {
+          element: '#note-form-cancel',
+          popover: {
+            title: 'Cancel',
+            description: 'If you want to cancel the note.',
+          },
+        },
+        {
+          element: '#theme-switch-button',
+          popover: {
+            title: 'Theme Switch',
+            description: 'You can switch between Light and Dark mode. (Default: System Preference)',
+          },
+        },
+        {
+          element: '#github-link',
+          popover: {
+            title: 'GitHub Link',
+            description:
+              'You can checkout the GitHub repo by clicking here. If you like the project you can also star &#11088; it there.',
+          },
+        },
+        {
+          popover: {
+            title: 'MarkUp',
+            description: 'You can play around the app and happy note taking :) .',
+          },
+        },
+      ],
+    });
+
+    driverObj.drive();
+  };
 
   return (
     <>
@@ -118,7 +129,7 @@ export const Topbar: React.FC<TopbarProps> = ({ setIsNavOpen }) => {
           variant='outline'
           size='sm'
           fontSize='sm'
-          onClick={() => driverObj.drive()}
+          onClick={handleStartTour}
         >
           Start Tour
         </Button>
